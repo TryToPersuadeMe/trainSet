138bfe55bdc28f79200c76fea17138284e0dcf2d
Dmitry <zombak1234@yandex.ru>
2021-03-10 19:05:10 +0300

    1

--- mainPage/mainPage/js/main.js
+++ mainPage/mainPage/js/main.js
@@ -1,1 +1,8 @@
-new WOW().init();
+wow = new WOW({
+  boxClass: "wow", // default
+  animateClass: "animated", // default
+  offset: 200, // default
+  mobile: true, // default
+  live: true, // default
+});
+wow.init();
